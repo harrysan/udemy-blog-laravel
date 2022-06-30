11fa8ab689fa93c86cfe6b0d005bd96846f3ba71
harrysan <harrysantoso8@yahoo.com>
2022-07-01 00:02:45 +0700

    Commit kedua sampai section 4

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -15,7 +16,12 @@ class AdminController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        $notification = array(
+            'message' => 'User Logout Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect('/login')->with($notification);
     } //End method
 
     public function profile()
@@ -24,7 +30,7 @@ class AdminController extends Controller
         $adminData = User::findOrFail($id);
 
         return view('admin.admin_profile_view', compact('adminData'));
-    } 
+    } //End Method
 
     public function EditProfile()
     {
@@ -32,5 +38,59 @@ class AdminController extends Controller
         $editData = User::findOrFail($id);
 
         return view('admin.admin_profile_edit', compact('editData'));
+    } //End Method
+
+    public function StoreProfile(Request $request)
+    {
+        $id = Auth::user()->id;
+        $data = User::findOrFail($id);
+        // dd($data);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->username = $request->username;
+
+        if($request->file('profile_image')) {
+            $file = $request->file('profile_image');
+
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['profile_image'] = $filename;
+        }
+
+        $data->save();
+
+        $notification = array(
+            'message' => 'Admin Profile Updated successfully',
+            'alert-type' => 'info'
+        );
+
+        return redirect()->route('admin.profile')->with($notification);
+    } //End Method
+
+    public function ChangePassword()
+    {
+        return view('admin.admin_change_password');
+    } //End Method
+
+    public function UpdatePassword(Request $request)
+    {
+        $validateData = $request->validate([
+            'oldpassword' => 'required',
+            'newpassword' => 'required',
+            'confirm_password' => 'required|same:newpassword'
+        ]);
+
+        $hashedPassword = Auth::user()->password;
+        if(Hash::check($request->oldpassword,$hashedPassword)) {
+            $users = User::findOrFail(Auth::id());
+            $users->password = bcrypt($request->newpassword);
+            $users->save();
+
+            session()->flash('message', 'Password Updated Successfully');
+            return redirect()->back();
+        } else {
+            session()->flash('message', 'Old Password is not match');
+            return redirect()->back();
+        }
     } //End Method
 }
--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,6 +1,7 @@
 @extends('admin.admin_master')
 
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 <div class="page-content">
     <div class="container-fluid">
@@ -12,7 +13,9 @@
 
                         <h4 class="card-title">Edit Profile Page</h4>
                         
-                        <form action="">
+                        <form method="POST" action="{{ route('store.profile') }}" enctype="multipart/form-data">
+                            @csrf
+
                             <div class="row mb-3">
                                 <label for="example-text-input" class="col-sm-2 col-form-label">Name</label>
                                 <div class="col-sm-10">
@@ -37,14 +40,14 @@
                             <div class="row mb-3">
                                 <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
                                 <div class="col-sm-10">
-                                    <input name="profile_image" class="form-control" type="file" id="profile_image">
+                                    <input name="profile_image" class="form-control" type="file" id="image">
                                 </div>
                             </div>
 
                             <div class="row mb-3">
                                 <label for="example-text-input" class="col-sm-2 col-form-label"></label>
                                 <div class="col-sm-10">
-                                    <img class="rounded avatar-lg" src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                                    <img id="showImage" class="rounded avatar-lg" src="{{ (!empty($editData->profile_image))? url('upload/admin_images/'.$editData->profile_image):url('upload/no_image.jpg') }}" alt="Card image cap">
                                 </div>
                             </div>
 
@@ -58,5 +61,19 @@
 
     </div>
 </div>
+
+<script type="text/javascript">
+
+    $(document).ready(function () {
+        $('#image').change(function (e) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+
+</script>
 
 @endsection
